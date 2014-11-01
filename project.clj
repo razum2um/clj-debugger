e76f68e0a41c77e186f72945292a35c9477c10e5
Vlad Bokov <razum2um@mail.ru>
2014-11-02 02:36:57 +0600

    start new iteration

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject debugger "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject debugger "0.1.1-SNAPSHOT"
+  :description "Debugger fro Clojure"
+  :url "https://github.com/razum2um/clj-debugger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
