741194e8058e908bc3730553109047ca49b8b062
Vlad Bokov <razum2um@mail.ru>
2014-11-02 02:35:41 +0600

    prepare for deploy

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject debugger "0.1.0-SNAPSHOT"
+(defproject debugger "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
