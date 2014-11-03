cced2c67b0b968d669438199f868668e9d4c5c32
Vlad Bokov <razum2um@mail.ru>
2014-11-04 04:31:12 +0600

    execution in proper ns, short/full sources, tests

--- src/debugger/core.clj
+++ src/debugger/core.clj
@@ -1,7 +1,9 @@
 (ns debugger.core
-  (:require clojure.reflect))
+  (:require [clojure.reflect]
+            [clojure.repl]))
 
 (declare ^:dynamic *locals*)
+(def ^:dynamic *code-context-lines* 5)
 
 (defmacro dbg
   [x]
@@ -10,8 +12,9 @@
        (println '~x "->" x#)
        x#)))
 
-(defn prompt-fn []
-  (printf "break %s=> " (ns-name *ns*)))
+(defn prompt-fn [signal-val]
+  (if-not @signal-val
+    (printf "break %s=> " (ns-name *ns*))))
 
 (defn print-table
   ([ks rows]
@@ -33,48 +36,117 @@
            (println (fmt-row " " " " " " row))))))
   ([rows] (print-table (keys (first rows)) rows)))
 
+(defn no-sources-found [fn-symbol]
+  (str "No source found for " fn-symbol))
+
 (defn help-message []
   (print-table
-    [:cmd :help]
-    [{:cmd "(h)" :help "prints this help"}
-     {:cmd "(w)" :help "prints code around breakpoint"}
-     {:cmd "(l)" :help "prints locals"}
-     {:cmd "(c)" :help "continues execution and preserves the result"}
-     {:cmd "^D"  :help "type Ctrl-D to exit break-repl and pass last result further"}
-     {:cmd ""    :help ""}
-     {:cmd ""    :help "you can also access locals directly and build sexp with them"}
-     {:cmd ""    :help "any last execution result (but `nil`) before exit will be passed further"}
-     {:cmd ""    :help "if last result is `nil` execution will continue normally"}]))
+    [:cmd :long :help]
+    [{:cmd "(h)" :long "(help)"      :help "prints this help"}
+     {:cmd "(w)" :long ""            :help "prints short code of breakpointed function"}
+     {:cmd ""    :long "(whereami)"  :help "prints full code of breakpointed function"}
+     {:cmd "(l)" :long "(locals)"    :help "prints locals"}
+     {:cmd "(c)" :long "(continue)"  :help "continues execution and preserves the result"}
+     {:cmd "(q)" :long "(quit)"      :help "or type Ctrl-D to exit break-repl and pass last result further"}
+     {:cmd ""    :long ""            :help ""}
+     {:cmd ""    :long ""            :help "you can also access locals directly and build sexp with them"}
+     {:cmd ""    :long ""            :help "any last execution result (but `nil`) before exit will be passed further"}
+     {:cmd ""    :long ""            :help "if last result is `nil` execution will continue normally"}]))
 
-(defn eval-fn [return-val cached-cont-val locals-fn cont-fn source form]
+(defn- format-line-with-line-numbers [short? break-line line line-number]
+  (cond
+    (= break-line line-number) (str "=> " line-number ": " line)
+    (and short? (<= line-number (- break-line *code-context-lines*))) nil
+    (and short? (>= line-number (+ break-line *code-context-lines*))) nil
+    :else (str "   " line-number ": " line)))
+
+
+(defn- map-numbered-source-lines [f fn-symbol]
+  (let [fn-source (clojure.repl/source-fn fn-symbol)
+        fn-source-lines (or (clojure.string/split fn-source #"\n") [])
+        fn-meta (-> fn-symbol find-var meta)
+        fn-source-start-line (or (:line fn-meta) 0)
+        line-numbers (map (partial + fn-source-start-line) (range))]
+    (mapv f fn-source-lines line-numbers)))
+
+
+(defn print-full-source [break-line fn-symbol]
+  (let [fn-meta (-> fn-symbol find-var meta)
+        format-fn (partial format-line-with-line-numbers false break-line)
+        lines (map-numbered-source-lines format-fn fn-symbol)]
+    (if (empty? lines)
+      (println (no-sources-found fn-symbol))
+      (do
+        (println)
+        (println (clojure.string/join "\n" lines) "\n")))))
+
+
+(defn print-short-source [break-line fn-symbol]
+  (let [fn-meta (-> fn-symbol find-var meta)
+        head (str "\nin: " (:ns fn-meta) "/" (:name fn-meta) " " (pr-str (:arglists fn-meta)) "\n")
+        format-fn (partial format-line-with-line-numbers true break-line)
+        lines (map-numbered-source-lines format-fn fn-symbol)
+        ;; _ (println "!!!" (count lines))
+        ]
+    (cond
+      (= 0 (count lines))
+        (println (no-sources-found fn-symbol))
+      (> (* 2 *code-context-lines*) (count lines))
+        (print-full-source break-line fn-symbol)
+      :else
+        (do
+          (println head)
+          (println (clojure.string/join "\n" (filter some? lines)))
+          (println)))))
+
+
+(defn eval-fn [break-ns break-line fn-symbol signal-val return-val cached-cont-val locals-fn cont-fn form]
   (do
-    (case (clojure.string/trim (str form))
-      "(h)" (do
-              (help-message)
-              (println))
-      "(w)" (do
-              (println)
-              (println source "\n"))
-      "(l)" (do
-              (println (locals-fn)))
-      "(c)" (do
-              (reset! cached-cont-val (cont-fn)))
+    (condp re-find (clojure.string/trim (str form))
+      #"\(h\)|\(help\)" (do
+                          (help-message)
+                          (println))
+
+      #"\(w\)" (do
+                 (print-short-source break-line fn-symbol))
+
+      #"\(whereami\)" (do
+                        (print-full-source break-line fn-symbol))
+
+      #"\(l\)|\(locals\)" (do
+                            (println (locals-fn)))
+
+      #"\(c\)|\(continue\)" (do
+                              (reset! cached-cont-val (cont-fn)))
+
+      #"\(q\)|\(quit\)" (do
+                          (reset! signal-val :stream-end)
+                          (println "Quitting debugger..."))
+
+      ;; TODO: require & use from ns + in *ns*
       (do
         (reset!
           return-val
-          (binding [*locals* (locals-fn)]
-            (eval
-              `(let ~(vec (mapcat #(list % `(*locals* '~%)) (keys *locals*)))
-                 ~form))))))))
+          (let [orig-ns (ns-name *ns*)]
+            (try
+              (in-ns break-ns)
+              (binding [*locals* (locals-fn)]
+                (eval
+                  `(let ~(vec (mapcat #(list % `(*locals* '~%)) (keys *locals*)))
+                     ~form)))
+              (finally (in-ns orig-ns)))))))))
+
 
 ;; TODO use readline/jline
-(defn read-fn [request-prompt request-exit]
+(defn read-fn [signal-val request-prompt request-exit]
   ;; (println "> Read-fn with" (pr-str request-prompt) "and" (pr-str request-exit))
   (or ({:line-start request-prompt :stream-end request-exit}
-       (clojure.main/skip-whitespace *in*))
+       (or (deref signal-val)
+           (clojure.main/skip-whitespace *in*)))
       (let [input (read)]
         (clojure.main/skip-if-eol *in*)
         input)))
+
 
 (defn caught-fn [e]
   (println "> Start caught-fn")
@@ -88,61 +160,43 @@
                       (str " " (if el (clojure.main/stack-element-str el) "[trace missing]"))))))))
 
 
-(defn- unmangle [s]
-  (clojure.string/replace s #"^(.+)\$(.+)\W?.*$" (fn [[_ ns-name fn-name]] (str ns-name "/" fn-name))))
+(defn unmangle [s]
+  (let [[ns-name fn-name & tail] (clojure.string/split s #"\$")]
+    (if (and ns-name fn-name)
+      (clojure.string/replace (str ns-name "/" fn-name) "_" "-"))))
 
-(defn- format-line-with-line-numbers [macro-line line line-number]
-  (if (= macro-line line-number)
-    (str "=> " line-number ": " line)
-    (str "   " line-number ": " line)))
 
 (defmacro break [& body]
   (let [env (into {} (map (fn [[sym bind]] [`(quote ~sym) (.sym bind)]) &env))
         ;; _ (println "form=" &form "meta" (meta &form))
-        macro-line (:line (meta &form))]
+        break-line (:line (meta &form))]
     `(let [return-val# (atom nil)
            cached-cont-val# (atom nil)
+           signal-val# (atom nil)
            cont-fn# #(identity ~@body)
            locals-fn# #(identity ~env)
 
            path-to-src# (-> (java.io.File. ".") .getCanonicalPath)
-           outer-fn-name# (-> (Throwable.) .getStackTrace first .getClassName unmangle symbol)
-           outer-fn-meta# (-> outer-fn-name# find-var meta)
-           outer-fn-line# (:line outer-fn-meta#)
-           outer-fn-path# (and outer-fn-meta# (str path-to-src# "/src/" (:file outer-fn-meta#) ":" outer-fn-line#))
+           outer-fn-symbol# (-> (Throwable.) .getStackTrace first .getClassName unmangle symbol)
+           s# (println "!!!" outer-fn-symbol#)
+           outer-fn-meta# (-> outer-fn-symbol# find-var meta)
+           outer-fn-path# (if outer-fn-meta#
+                            (str path-to-src# "/src/" (:file outer-fn-meta#) ":" (:line outer-fn-meta#))
+                            (no-sources-found outer-fn-symbol#))
 
-           outer-fn-source# (clojure.string/trim (clojure.repl/source-fn outer-fn-name#))
-           source# (clojure.string/join
-                     "\n"
-                     (mapv (partial format-line-with-line-numbers ~macro-line)
-                           (or (clojure.string/split outer-fn-source# #"\n") [])
-                           (map (partial + outer-fn-line#) (range))))
+           macro-eval-fn# (partial eval-fn (ns-name (or (:ns outer-fn-meta#) *ns*)) ~break-line outer-fn-symbol# signal-val# return-val# cached-cont-val# locals-fn# cont-fn#)
+           macro-read-fn# (partial read-fn signal-val#)
+           macro-prompt-fn# (partial prompt-fn signal-val#)]
 
-           macro-eval-fn# (partial eval-fn return-val# cached-cont-val# locals-fn# cont-fn# source#)]
-       (println "\nBreak from:" outer-fn-path# "(type \"(h)\" for help)\n")
-       (println source# "\n")
+       (println "\nBreak from:" outer-fn-path# "(type \"(help)\" for help)")
+       (print-short-source ~break-line outer-fn-symbol#)
        (clojure.main/repl
-         :prompt prompt-fn
+         :prompt macro-prompt-fn#
          :eval macro-eval-fn#
-         :read read-fn
+         :read macro-read-fn#
          :caught caught-fn)
        (or
          (deref return-val#)
          (deref cached-cont-val#)
          (cont-fn#)))))
-
-(defn foo [& args]
-  (let [x "world"
-        y '(1 2)
-        z (Object.)
-        ret (break (inc 42))]
-    (println "Exit foo with" ret)))
-
-(defn bar [multi]
-  (let [my-fn (dbg (break (fn inner [x] (* multi x))))]
-    (map my-fn (range 2))))
-
-(defn qux [multi]
-  (let [my-fn (fn inner [x] (break (* multi x)))]
-    (map my-fn (range 2))))
 
--- test/debugger/core_test.clj
+++ test/debugger/core_test.clj
@@ -4,4 +4,26 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(def wow 1)
+
+(defn foo [& args]
+  (let [a [1 2]
+        b #{3 4}
+        c {:k "v"}
+        d nil
+        e (fn [] nil)
+        x "world"
+        y '(8 9)
+        z (Object.)
+        ret (break (inc 42))]
+    (println "Exit foo with" ret)))
+
+(defn bar [multi]
+  (let [my-fn (dbg (break (fn inner [x] (* multi x))))]
+    (map my-fn (range 2))))
+
+(defn qux [multi]
+  (let [my-fn (fn inner [x] (break (* multi x)))]
+    (map my-fn (range 2))))
