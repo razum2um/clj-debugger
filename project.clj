f31ef0b32bc926379f8543f84b8c3e8a7a18dfef
Vlad Bokov <razum2um@mail.ru>
2015-03-28 12:55:04 +0600

    start 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject debugger "0.1.6-SNAPSHOT"
+(defproject debugger "0.1.7-SNAPSHOT"
   :description "Debugger for Clojure"
   :url "https://github.com/razum2um/clj-debugger"
   :license {:name "Eclipse Public License"
