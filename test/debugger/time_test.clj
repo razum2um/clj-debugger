1d80201c23854ec8bb91e890102b232f5eb6346b
Michael Griffiths <mikey@cich.li>
2015-03-29 22:21:33 +0100

    Remove extraneous empty vectors from tests

--- test/debugger/time_test.clj
+++ test/debugger/time_test.clj
@@ -3,21 +3,21 @@
   (:require [debugger.time :refer :all]
             [clojure.test :refer :all]))
 
-(deftest now-test []
+(deftest now-test
   (is (= (.getTime (Date.)) (.getTime (now)))))
 
 
-(deftest minus-test []
+(deftest minus-test
   (is (= (-> (Date.).getTime (- 2000) (Date.) .getTime)
          (-> (now) (minus (seconds 2)) .getTime))))
 
-(deftest interval-test []
+(deftest interval-test
   (is (= 4 (-> (now)
                (minus (seconds 4))
                (interval (now))
                (in-seconds)))))
 
-(deftest compatibility-test []
+(deftest compatibility-test
   (are [last-quit-seconds-ago skip-repl-if-last-quit-ago check]
     (is (= check (->> (now) (interval (minus (now) (seconds last-quit-seconds-ago))) in-seconds (< skip-repl-if-last-quit-ago))))
     4 2 true
